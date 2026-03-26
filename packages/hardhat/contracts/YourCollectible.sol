5ee4770e37877adba34df5076e5317fc29c8f42e
KudraDube <kudradube@gmail.com>
2026-03-26 10:49:17 +0200

    fix: update YourCollectible contract

--- packages/hardhat/contracts/YourCollectible.sol
+++ packages/hardhat/contracts/YourCollectible.sol
@@ -5,57 +5,18 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBaseV2.sol";
-import "@chainlink/contracts/src/v0.8/vrf/interfaces/VRFCoordinatorV2Interface.sol";
 
-import "./VRFConsumerBaseV2.sol";
-import "./interfaces/VRFCoordinatorV2Interface.sol";
-
-contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable, VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
-
-    // Fixed Subscription ID (using the 19-digit version from the dashboard)
-    uint64 private immutable i_subscriptionId = 2259864542842053642; 
-    bytes32 private immutable i_gasLane = 0x474e34a077df58807dbe9c96d3c009b23b3c6d0cce433e59bbf5b34f823bc56c;
-    uint32 private immutable i_callbackGasLimit = 500000;
-    uint16 private constant REQUEST_CONFIRMATIONS = 3;
-    uint32 private constant NUM_WORDS = 1;
+contract YourCollectible is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
 
     uint256 public tokenIdCounter;
-    mapping(uint256 => uint256) public s_requestIdToTokenId;
-    mapping(uint256 => uint256) public s_tokenIdToRandomValue;
 
-    constructor() 
-        ERC721("YourCollectible", "YCB") 
-        Ownable(msg.sender)
-        VRFConsumerBaseV2(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625) 
-    {
-        i_vrfCoordinator = VRFCoordinatorV2Interface(0x8103B0A8A00be2DDC778e6e7eaa21791Cd364625);
-    }
+    constructor() ERC721("YourCollectible", "YCB") Ownable(msg.sender) {}
 
-    function mintItem(address to, string memory uri) public returns (uint256 requestId) {
-        requestId = i_vrfCoordinator.requestRandomWords(
-            i_gasLane,
-            i_subscriptionId,
-            REQUEST_CONFIRMATIONS,
-            i_callbackGasLimit,
-            NUM_WORDS
-        );
-        
+    function mintItem(address to, string memory uri) public returns (uint256) {
         tokenIdCounter++;
-        uint256 tokenId = tokenIdCounter;
-        _safeMint(to, tokenId);
-        _setTokenURI(tokenId, uri);
-
-        s_requestIdToTokenId[requestId] = tokenId;
-        return requestId;
-    }
-
-    // FIXED: Removed the extra space/hidden char causing the ParserError
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
-        uint256 tokenId = s_requestIdToTokenId[requestId];
-        s_tokenIdToRandomValue[tokenId] = randomWords[0];
+        _safeMint(to, tokenIdCounter);
+        _setTokenURI(tokenIdCounter, uri);
+        return tokenIdCounter;
     }
 
     function _baseURI() internal pure override returns (string memory) {
